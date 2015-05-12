3fd3bdc7f64ebc3dd055f1e317f87bf93528a9b1
Vitor Leal <vitorleal1@gmail.com>
2015-05-12 09:40:03 -0300

    improve the install file

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
+files=(
+  "./.aliases"
+  "./.bash_profile"
+  "./.bash_prompt"
+  "./.bashrc"
+  "./.exports"
+  "./.gitcofig"
+  "./.gitignore"
+  "./.screenrc"
+  "./.vim"
+  "./.vimrc"
+)
+
 echo "Puting dotfiles in your home path ~/"
-for file in ./.{aliases,bash_profile,bash_prompt,bashrc,exports,gitconfig,gitignore,screenrc,vim,vimrc}; do
+for file in ${files[@]}; do
     if [[ $(file $file | awk '{print $2}') == "directory" ]]; then
       [ -r "$file" ] && cp -r "$file" ~/ && echo "copied folder $file"
 
