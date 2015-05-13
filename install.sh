22a1d595f1b6ed0685668ee39ef486a50a4c9f20
Vitor Leal <vitorleal1@gmail.com>
2015-05-13 14:36:14 -0300

    add vundle install to install.sh

--- install.sh
+++ install.sh
@@ -39,3 +39,19 @@ else
   echo "  - Done installing Homebrew"
 fi;
 
+
+# Preparing VIM and Vundle Plugins
+echo "• Preperaing Vim and Vundle Plugins"
+
+if [[ -r "$HOME/.vim/bundle/Vundle.vim" ]]; then
+  echo "  - Vundle already installed. Lets install the plugins"
+  vim +PluginInstall +qall
+  echo "  - Done installing the Plugins"
+
+else
+  echo "  - Lets install Vundle Plugin Manager"
+  git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+  echo " - Done installing the Plugins"
+fi;
+
